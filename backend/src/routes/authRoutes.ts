3b0143c258c14cdd45814e5f54a214c4e5bda598
Dharyl Carry S. Almora <37977733+dharylovey@users.noreply.github.com>
2024-09-27 22:31:32 +0800

    fix: bug in login endpoint

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+    forgotPassword,
   login,
   logout,
   register,
@@ -15,5 +16,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.post("/verify-email", verifyEmail);
+
+router.post("/forgot-password", forgotPassword);
 
 export default router;
