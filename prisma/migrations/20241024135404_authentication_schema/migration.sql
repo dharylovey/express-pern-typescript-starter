b11c7b9ebd2c6f143791d8333dcede3c4ea0dde5
Dharyl Carry S. Almora <dharylovey@gmail.com>
2024-11-21 19:36:24 +0800

    move src to root directory

---
+++ prisma/migrations/20241024135404_authentication_schema/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "lastLogin" DATETIME,
+    "verificationCode" TEXT,
+    "verificationCodeExpires" DATETIME,
+    "resetPasswordToken" TEXT,
+    "resetPasswordExpires" DATETIME,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
